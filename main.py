ddd373b34bd5dd14f2381512fcc63d6b682e90c7
Michel Llorens <mllorens@dcc.uchile.cl>
2016-04-19 11:03:05 -0300

    Add the images for tests.

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 
 diffuse = Diffuse.Diffuse(functions.h1)
 
-image = io.imread('Images/frutas.jpg')
-diffuse_image = diffuse.apply('Images/frutas.jpg', 0.5, 20, 40)
+image = io.imread('Images/lenna.png')
+diffuse_image = diffuse.apply('Images/lenna.png', 0.25, 50, 20)
 
 fig, (img, dif) = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
 
-img.imshow(image)
-dif.imshow(diffuse_image)
+img.imshow(image, cmap=plt.cm.gray)
+dif.imshow(diffuse_image, cmap=plt.cm.gray)
 
 fig.subplots_adjust(wspace=0.02, hspace=0.02, top=0.9, bottom=0.02, left=0.02, right=0.98)
 
